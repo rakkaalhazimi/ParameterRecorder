40fd85ad838a23401264000892c895a9593d476d
unknown <rakkakeren@gmail.com>
2023-01-08 18:45:09 +0700

    Re initialize app with blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,34 +1,28 @@
-import os
-import platform
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from flask_migrate import Migrate
-import config
+from flask_sqlalchemy import SQLAlchemy
+
+from app import config
+
 
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
+app.config.from_object(config.Config)
+
+db = SQLAlchemy()
+db.init_app(app)
 migrate = Migrate(app, db, compare_type=True)
 
-
-# Import configuration profile based on FLASK_ENV variable - defaults to Production
-if os.environ.get('FLASK_ENV') == 'development':
-    app.config.from_object(config.DevelopmentConfig)
-elif os.environ.get('FLASK_ENV') == 'testing':
-    app.config.from_object(config.TestingConfig)
-else:
-    app.config.from_object(config.ProductionConfig)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
-# Select Environment Type (Production / Development)
-OS_NAME = platform.system()
-
-if OS_NAME == "Linux":
-    app.config["ENV"] = "production"
-else:
-    app.config["ENV"] = "development"
+from app.base.views import base
+from app.auth.views import auth
+app.register_blueprint(base)
+app.register_blueprint(auth)
 
 
-
-# Import routes here
-from app.routes import *
+with app.app_context():
+    db.create_all()
--- app/models.py
+++
@@ -1,7 +0,0 @@
-from . import db
-
-class Test(db.Model):
-    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
-    name = db.Column(db.String(255), nullable=False)
-
-db.create_all()
