1594bd4967cd30f897bcfdc4ece168131ed5fedc
unknown <rakkakeren@gmail.com>
2023-01-17 22:46:56 +0700

    add view to show and create projects

--- app/base/views.py
+++ app/base/views.py
@@ -1,11 +1,32 @@
-import flask
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required
+from pony import orm
+
+from app.base import models
 
 
-base = flask.Blueprint("base", __name__)
+base = Blueprint("base", __name__)
 
 
 @base.route("/")
 @login_required
 def home():
-    return "Hello world"
+    projects = orm.select(project.name for project in models.Projects)
+    return render_template("index.html", projects=projects)
+
+
+@base.route("/create", methods=["GET", "POST"])
+def create_project():
+
+    if request.method == "POST":
+        project_name = request.form["name"]
+        
+        if project_name.strip():
+            models.Projects(name=project_name)
+            orm.commit()
+            flash("Project created")
+            return redirect(url_for("base.home"))
+        
+        flash("failed to create project")
+
+    return render_template("project_create.html")
