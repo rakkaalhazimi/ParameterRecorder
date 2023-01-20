97e14cdebf69744966f1ba965a0071194dce9c4a
unknown <rakkakeren@gmail.com>
2023-01-20 19:33:25 +0700

    add register view

--- app/base/views.py
+++ app/base/views.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required
 from pony import orm
 
-from app.base import models
+from app.base import models, services
 
 
 base = Blueprint("base", __name__)
@@ -15,9 +15,8 @@ def home():
     return render_template("index.html", projects=projects)
 
 
-@base.route("/create", methods=["GET", "POST"])
+@base.route("/projects/create", methods=["GET", "POST"])
 def create_project():
-
     if request.method == "POST":
         project_name = request.form["name"]
         
@@ -30,3 +29,25 @@ def create_project():
         flash("failed to create project")
 
     return render_template("project_create.html")
+
+
+@base.route("/projects/update/<int:project_id>", methods=["GET", "POST"])
+def update_project(project_id: int):
+    project = models.Projects.get(id=project_id)
+    context = {
+        "id":project_id, 
+        "name": project.name
+    }
+
+    if request.method == "POST":
+        project_name = request.form["name"]
+
+        if project_name.strip():
+            project.name = project_name
+            orm.commit()
+            flash("Project updated")
+            return redirect(url_for("base.home"))
+        
+        flash("failed to update project")
+    
+    return render_template("project_update.html", context=context)
