3992ae415af60584344ec5bdeb87ea7abf50468a
unknown <rakkakeren@gmail.com>
2023-01-21 10:52:24 +0700

    add delete project and get record services
    add type hinting in add_project

--- app/base/services.py
+++ app/base/services.py
@@ -1,6 +1,6 @@
 from pony import orm
 
-from .models import Projects
+from .models import Projects, Records
 
 
 def get_all_projects():
@@ -9,7 +9,15 @@ def get_all_projects():
 def get_project_by_id(id: int):
     return Projects.get(id=id)
 
-def add_project(current_user, name):
+def add_project(current_user: int, name: str):
     Projects(name=name, user=current_user)
     orm.commit()
     return True
+
+def delete_project(id: int):
+    Projects.get(id=id).delete()
+    orm.commit()
+
+
+def get_records_by_project(project_id: str):
+    return Records.select(lambda record: record.project.id == project_id)
