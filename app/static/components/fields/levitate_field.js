d7516568df12df9bcf1f6e2f642812a22f8ce754
unknown <rakkakeren@gmail.com>
2023-04-13 06:10:05 +0700

    feat: reactive field onFocus

--- app/static/components/fields/levitate_field.js
+++ app/static/components/fields/levitate_field.js
@@ -1,3 +1,5 @@
+const {ref, reactive} = Vue
+
 export default {
     props: {
         type: String,
@@ -7,15 +9,38 @@ export default {
     },
 
     setup(props) {
+
+        let state = reactive({isFocus: false})
+
+        function fieldFocusIn(event) {
+            state.isFocus = true
+        }
+
+        function fieldFocusOut(event) {
+            state.isFocus = false
+        }
+
+        return {fieldFocusIn, fieldFocusOut, state}
     },
 
     template: /*html*/`
         <div class="field">
-            <label class="field__label">{{ label }}</label>
+            <label class="field__label" :class="{'field__label--focus': state.isFocus}">
+                {{ label }}
+            </label>
             <br/>
-            <div class="field__wrapper">
-                <input :type="type" :value="value" :name="name" class="field__input" />
-                <span class="field__icon-wrapper">
+            <div class="field__wrapper" :class="{'field__wrapper--focus': state.isFocus}">
+                <input
+                    @focusin="fieldFocusIn"
+                    @focusout="fieldFocusOut"
+                    :type="type"
+                    :value="value"
+                    :name="name"
+                    class="field__input"
+                />
+                <span
+                    class="field__icon-wrapper"
+                    :class="{'field__icon-wrapper--focus': state.isFocus}">
                     <slot name="icon"></slot>
                 </span>
             </div>
