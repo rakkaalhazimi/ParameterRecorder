54d5242b54ee97c4d1ea4937d9485ddb9ed2f7d4
unknown <rakkakeren@gmail.com>
2023-04-20 00:22:25 +0700

    add: view and delete link in project card

--- app/static/components/cards/ProjectCard.js
+++ app/static/components/cards/ProjectCard.js
@@ -3,6 +3,8 @@ import TrashSolid from "../icons/TrashSolid.js"
 export default {
     props: {
         title: String,
+        viewLink: String,
+        deleteLink: String
     },
 
     components: {
@@ -13,13 +15,17 @@ export default {
     },
 
     template: /*html*/`
-        <div class="project-card">
-            <div class="project-card__header">
-                <h3 class="project-card__title">{{ title }}</h3>
-                <TrashSolid class="project-card__delete-icon"></TrashSolid>
-            </div>
+        <a :href="viewLink">
+            <div class="project-card">
+                <div class="project-card__header">
+                    <h3 class="project-card__title">{{ title }}</h3>
+                    <a :href="deleteLink">
+                        <TrashSolid class="project-card__delete-icon"></TrashSolid>
+                    </a>
+                </div>
 
-            <p class="project-card__desc">There are 0 records</p>
-        </div>
+                <p class="project-card__desc">There are 0 records</p>
+            </div>
+        </a>
     `
 }
