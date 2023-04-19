2e445ed5670de8b233d09a64efe8a239e4693a66
unknown <rakkakeren@gmail.com>
2023-04-20 00:15:12 +0700

    add: trash icon in project card

--- app/static/components/cards/ProjectCard.js
+++ app/static/components/cards/ProjectCard.js
@@ -1,9 +1,12 @@
+import TrashSolid from "../icons/TrashSolid.js"
+
 export default {
     props: {
         title: String,
     },
 
     components: {
+        "TrashSolid": TrashSolid,
     },
 
     setup(props) {
@@ -11,7 +14,11 @@ export default {
 
     template: /*html*/`
         <div class="project-card">
-            <h3 class="project-card__title">{{ title }}</h3>
+            <div class="project-card__header">
+                <h3 class="project-card__title">{{ title }}</h3>
+                <TrashSolid class="project-card__delete-icon"></TrashSolid>
+            </div>
+
             <p class="project-card__desc">There are 0 records</p>
         </div>
     `
